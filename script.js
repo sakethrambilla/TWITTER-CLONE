c9fd0b7da95176c977fddd3fb578d2b344557a85
indrarjun <billasaketh@gmail.com>
2022-10-26 22:24:43 +0530

    user profile updated

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ const newsFeedPage = document.querySelector('.feeds-page');
 const loginModel= document.querySelector('.login-model');
 const modelX =document.querySelector('.login-model i');
 const loginFormBtn= document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX =document.querySelector(".modal-header i");
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector(".modal-input");
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper =document.querySelector('.sidebar-wrapper');
+const xBtn = document.querySelector(".sidebar-header i");
 
 /***************************************************/
 // Main Page
@@ -56,4 +67,52 @@ loginFormBtn.addEventListener('click',() =>{
         loginModel.style.display='block';
         
     }
+})
+
+
+
+// News Feed Page
+// Post Modal
+postBtn.addEventListener('click', () => {
+    modal.style.display ='block';
+    modalWrapper.classList.add("modal-wrapper-display");
+    if(modalInput.value !==''){
+        modalInput.value ="";
+        changeOpacity(0.5);
+    }
+}); 
+
+const changeOpacity = (x) =>{
+    modalPostBtn.style.opacity =x;
+    modalFooterPlus.style.opacity =x;
+};
+
+postModalX.addEventListener('click', () =>{
+    modal.style.display = 'none';
+    modalWrapper.classList.remove("modal-wrapper-display");
+});
+
+modalInput.addEventListener("keypress" , e =>{
+    if(e.target.value!==''){
+        changeOpacity(1);
+    }
+});
+
+modalInput.addEventListener("blur", (e) => {
+    if(e.target.value === ""){
+        changeOpacity(0.5);
+    }
+});
+
+// sidebar
+
+user.addEventListener('click', () =>{
+    sidebar.classList.add('sidebar-display');
+    sidebarWrapper.classList.add('sidebar-wrapper-display');
+});
+
+
+xBtn.addEventListener("click" , ()=>{
+    sidebar.classList.remove("sidebar-display");
+    sidebarWrapper.classList.remove("sidebar-wrapper-display");
 })
